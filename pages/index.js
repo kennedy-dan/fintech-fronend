c60e12bed2b4dd9817c40e456b3da7c94ddcde3b
Kennedy Daniel <kenidankid@yahoo.com>
2023-08-04 18:22:34 +0100

    dashboard redirect

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,21 @@
 import Image from 'next/image'
 import { Inter } from 'next/font/google'
 import dynamic from 'next/dynamic'
+import { useEffect } from 'react'
+import { useRouter } from 'next/router'
+
+
  
 
 const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
+ const router = useRouter()
+
+  useEffect(() => {
+    router.push('/dashboard')
+  }, [router])
+  
   return (
     <main
       className={`flex min-h-screen flex-col items-center justify-between p-24 ${inter.className}`}
